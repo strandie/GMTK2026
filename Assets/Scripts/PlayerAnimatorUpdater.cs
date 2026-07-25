713b646e0c96a2f51c605c614f34139d6e216467
Felix Zhu <felix.zhu@duke.edu>
2026-07-25 13:35:58 -0700

    Added dash trail effect to indicate speed

--- Assets/Scripts/PlayerAnimatorUpdater.cs
+++ Assets/Scripts/PlayerAnimatorUpdater.cs
@@ -6,6 +6,10 @@ public class PlayerAnimatorUpdater : MonoBehaviour
     private PlayerMovement playerMovement;
     private Rigidbody2D rb;
     private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    public Gradient DashSpeedHueShift;
+    public float MaxHueSpeedValue = 10f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +17,7 @@ public class PlayerAnimatorUpdater : MonoBehaviour
         playerMovement = GetComponent<PlayerMovement>();
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -23,5 +28,14 @@ public class PlayerAnimatorUpdater : MonoBehaviour
         animator.SetBool("IsDashing", playerMovement.IsDashing());
         animator.SetBool("WallSlide", playerMovement.whichWallSliding() != 0);
         animator.SetFloat("MovementAngle", Mathf.Rad2Deg * Mathf.Atan2(rb.linearVelocityY, Mathf.Abs(rb.linearVelocityX)));
+
+        if(playerMovement.IsDashing())
+        {
+            spriteRenderer.color = DashSpeedHueShift.Evaluate(rb.linearVelocity.sqrMagnitude / MaxHueSpeedValue);
+        }
+        else
+        {
+            spriteRenderer.color = Color.white;
+        }
     }
 }
