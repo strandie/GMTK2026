fa9432213d656cd9a4ee537cdba582e54bff3738
Felix Zhu <felix.zhu@duke.edu>
2026-07-24 22:29:07 -0700

    Added TMP and timer with font

---
+++ Assets/Scripts/TimerManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using TMPro;
+
+public class TimerManager : MonoBehaviour
+{
+    public static TimerManager Instance {get; private set;}
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public TextMeshProUGUI timerText;
+    private float timer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timer = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timerText.text = timer.ToString("00.00");
+    }
+
+    public void SetTimer(float val)
+    {
+        timer = val;
+    }
+    public void AddToTimer(float val)
+    {
+        timer += val;
+    }
+    public void SubtractFromTimer(float val)
+    {
+        timer -= val;
+    }
+}
