5fe65712ceeb8d1aeba1bf00aa8ca6702d390189
Felix Zhu <felix.zhu@duke.edu>
2026-07-25 14:24:13 -0700

    Added timer effects

--- Assets/Scripts/Timer/TimerManager.cs
+++ Assets/Scripts/Timer/TimerManager.cs
@@ -4,7 +4,7 @@ using TMPro;
 public class TimerManager : MonoBehaviour
 {
     public static TimerManager Instance {get; private set;}
-    [SerializeField] private TimerTextSpawner timerTextSpawner;
+    
 
     private void Awake()
     {
@@ -21,10 +21,41 @@ public class TimerManager : MonoBehaviour
 
     public TextMeshProUGUI timerText;
     private float timer;
+    [SerializeField] private TimerTextSpawner timerTextSpawner;
 
+    [Header("Color Shift")]
+    public float maxTimerValue = 60f;
+    public Gradient timerColorOverTime;
+
+
+    [Header("Rotation Jitter")]
+    public float rotateJitterMaxAngle = 10f;
+    public float rotateReturnSpeed = 0.1f;
+    public float rotateToSpeed = 1f;
+    private float targetRotation = 0f;
+    private float currRotation = 0f;
+
+
+    [Header("Size Jitter")]
+    public float sizeJitterStrength = 0.5f;
+    public float sizeJitterDecayRate = 0.1f;
+    public float sizeJitterDecayOffset = 0.5f;
+    public float sizeGrowSpeed = 1f;
+    public float sizeShrinkSpeed = 0.1f;
+    private float jitterSizeIncreaseFunction(float inputSize)
+    {
+        inputSize = Mathf.Max(1f, inputSize); // safeguard
+        // Basically don't want the jitter to increase indefinitely, so using a decaying function
+        float output = sizeJitterStrength / (sizeJitterDecayRate * inputSize + sizeJitterDecayOffset) + 1f;
+        return output;
+    }
+    private float originalScale;
+    private float sizeScale = 1f;
+    private float targetSizeScale = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        originalScale = timerText.transform.localScale.x;
         timer = 10f;
     }
 
@@ -33,6 +64,22 @@ public class TimerManager : MonoBehaviour
     {
         timer -= Time.deltaTime;
         timerText.text = timer.ToString("00.00");
+        timerText.color = timerColorOverTime.Evaluate(timer / maxTimerValue);
+
+        sizeScale = Mathf.MoveTowards(sizeScale, targetSizeScale,
+                        targetSizeScale > sizeScale ? sizeGrowSpeed : sizeShrinkSpeed);
+        if (sizeScale >= targetSizeScale) targetSizeScale = 1f; // Once reaching target, go to baseline
+        timerText.transform.localScale = Vector3.one * sizeScale * originalScale;
+
+        bool rotatingToBaseline = targetRotation == 0f;
+        currRotation = Mathf.MoveTowards(currRotation, targetRotation,
+                        rotatingToBaseline ? rotateReturnSpeed : rotateToSpeed);
+        if(!rotatingToBaseline && Mathf.Abs(currRotation) >= Mathf.Abs(targetRotation))
+        {
+            // finished rotation
+            targetRotation = 0f;
+        }
+        timerText.transform.rotation = Quaternion.Euler(Vector3.forward * currRotation);
     }
 
     public void SetTimer(float val)
@@ -43,10 +90,14 @@ public class TimerManager : MonoBehaviour
     {
         timer += val;
         timerTextSpawner.SpawnTextNumber(val);
+        targetSizeScale = jitterSizeIncreaseFunction(sizeScale);
+        targetRotation = Random.Range(-rotateJitterMaxAngle, rotateJitterMaxAngle);
     }
     public void SubtractFromTimer(float val)
     {
         timer -= val;
         timerTextSpawner.SpawnTextNumber(-val);
+        targetSizeScale = jitterSizeIncreaseFunction(sizeScale);
+        targetRotation = Random.Range(-rotateJitterMaxAngle, rotateJitterMaxAngle);
     }
 }
